bf21d6e33fa1cf23de32ccd6096ec3e7f885a8e7
harry.wong <hwong23@gmail.com>
2025-01-01 17:48:09 -0500

    2.7.10

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
-# # Setup display
-# Xvfb :99 &
-# export DISPLAY=:99
+#!/bin/sh -l
 
-# exec $@
+exec $@
 
-# pkill -f 'Xvfb :99'
-
+echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
